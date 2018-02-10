433e4c447f71ffefb17b7645cd9c5c22b0ebbca7
R45H <gmr45h@gmail.com>
2018-02-10 22:49:22 +0300

    Fix jumping header when the body gets noscroll state

--- app/src/global/functions.js
+++ app/src/global/functions.js
@@ -21,17 +21,21 @@ function getScrollbarWidth() {
 /* Сделать боди фиксированным */
 function toggleBodyScroll(noscroll) {
 	var
+		noScrollClass = 'noscroll',
+		scrollWidth = getScrollbarWidth(),
 		$body = $('body'),
-		noScrollClass = 'noscroll';
+		$header = $('.header');
 
 	if (noscroll === false) {
 		$body
 			.css('padding-right', '')
 			.removeClass(noScrollClass);
+		$header.css('padding-right', '');
 	} else {
 		$body
-			.css('padding-right', getScrollbarWidth())
+			.css('padding-right', scrollWidth)
 			.addClass(noScrollClass);
+		$header.css('padding-right', scrollWidth);
 	}
 }
 /* ========== */
